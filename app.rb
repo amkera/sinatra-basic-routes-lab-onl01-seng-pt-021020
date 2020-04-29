134330ee9f9fe8ed262f919f868b3b029c1de896
Amber Kerawala <amber_kerawala@berkeley.edu>
2020-04-29 22:55:22 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,6 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
   get '/name' do
+    "My name is #{name}"
+    erb: ''
+  end 
+  
+  get '' do 
+  end 
+  
+  get '' do 
+  end 
     
 end
